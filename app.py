db56a7990be5c500af7e4a21caa4cad1516ef05c
JesusPaz <jeherpabo@gmail.com>
2019-11-05 14:16:24 -0500

    Try to solve travis problems

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.add_api('swagger.yml')
 
 @app.route('/')
 def home():
-    return {"Hello": "To Home"}
+    return "Welcome to my API"
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(debug=True)
