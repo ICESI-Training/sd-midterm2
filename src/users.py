1b4ffd25f5f12ebeb7fb45add636656c2e0d351e
Jesus Paz <jeherpabo@gmail.com>
2019-11-04 22:51:08 -0500

    Connection with github, only need to check what happen if the user dont exists and some tests using travis

--- src/users.py
+++ src/users.py
@@ -1,31 +1,22 @@
 import pymongo
 from bson.json_util import dumps
-
-users = {
-    "JesusPaz": {
-        "name": "Jesus 1",
-        "numRepos": 2
-    },
-    "Jesus2": {
-        "name": "Jesus 2",
-        "numRepos": 3
-    },
-    "Jesus3": {
-        "name": "Jesus 3",
-        "numRepos": 4
-    }
-}
+import requests
+import json
 
 MongoDB_URI = "mongodb+srv://jesuspaz:admin@cluster0-sobp3.mongodb.net/sd?retryWrites=true&w=majority"
 client = pymongo.MongoClient(MongoDB_URI)
 db = client.get_default_database()
 users = db['users']
 
+urlBase = 'https://api.github.com'
 
 # Return all the users from the database, you can access using a GET /users
 def read():
     return dumps(users.find())
 
 def insert(name):
-    result = users.insert_one({"name": name, "numRepos": 4})
+    r = requests.get(urlBase+"/users/"+name)
+    response = json.loads(r.content)
+    numRepos = response["public_repos"]
+    result = users.insert_one({"name": name, "numRepos": numRepos})
     return dumps(result.inserted_id)
