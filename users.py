a62af8947e256502083425e63a6efbe744b8a54d
Jesus Paz <jeherpabo@gmail.com>
2019-11-07 19:36:42 -0500

    Add files changes

--- users.py
+++ users.py
@@ -5,12 +5,12 @@ import json
 import dns
 import os
 
-# MongoDB_URI = "mongodb+srv://admin:admin@cluster0-n5sgi.mongodb.net/sd-db?retryWrites=true&w=majority"
+MongoDB_URI = "mongodb+srv://admin:admin@cluster0-n5sgi.mongodb.net/sd-db?retryWrites=true&w=majority"
 # MongoDB_URI = "mongodb://admin:admin123@ds241278.mlab.com:41278/heroku_2b3pbkcr?retryWrites=true&w=majority"
 
-client = pymongo.MongoClient(os.environ["MongoDB_URI"])
+# client = pymongo.MongoClient(os.environ["MongoDB_URI"])
 
-# client = pymongo.MongoClient(MongoDB_URI)
+client = pymongo.MongoClient(MongoDB_URI)
 
 db = client.get_default_database()
 users = db['users']
