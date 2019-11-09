5020d5871764d341c984ee4b117890bf6d58175d
Jesus Paz <jeherpabo@gmail.com>
2019-11-09 12:40:00 -0500

    Final server and test to API, also include images to the readme

--- src/users.py
+++ src/users.py
@@ -5,26 +5,64 @@ import json
 import dns
 import os
 
-MongoDB_URI = "mongodb+srv://admin:admin@cluster0-n5sgi.mongodb.net/sd-db?retryWrites=true&w=majority"
-# MongoDB_URI = "mongodb://admin:admin123@ds241278.mlab.com:41278/heroku_2b3pbkcr?retryWrites=true&w=majority"
-
-# client = pymongo.MongoClient(os.environ["MongoDB_URI"])
-
-client = pymongo.MongoClient(MongoDB_URI)
+url = os.environ.get("mongoURL")+"/sd-db?retryWrites=true&w=majority"
+client = pymongo.MongoClient(url)
 
 db = client.get_default_database()
 users = db['users']
 
 urlBase = 'https://api.github.com'
 
+
 # Return all the users from the database, you can access using a GET /users
 def read():
     return dumps(users.find())
 
+
 def insert(name):
-    r = requests.get(urlBase+"/users/"+name)
-    response = json.loads(r.content)
-    numRepos = response["public_repos"]
-    result = users.insert_one({"name": name, "numRepos": numRepos})
-    data = {"id" : result.inserted_id, "numRepos" : numRepos}
-    return dumps(data)
+    # User name is empty
+    if name.strip() != "":
+        # Users exists in the data base
+
+        if users.find({"name": name}).count() == 0:
+
+            r = requests.get(urlBase + "/users/" + name)
+            # Users is a github user
+            if r.status_code == 200:
+                response = json.loads(r.content)
+                numRepos = response["public_repos"]
+                result = users.insert_one({"name": name, "numRepos": numRepos})
+                data = {"id": result.inserted_id, "numRepos": numRepos}
+                return dumps(data)
+            else:
+                return 'Bad request', 400, {
+                    'exists-gh-error':
+                    'The name of the user given is not in the GitHub database'
+                }
+        else:
+            return 'Bad request', 400, {
+                'exists-error': 'The users exists in the database'
+            }
+    else:
+        return 'Bad request', 400, {
+            'empty-error': 'The name of the user given can not be empty'
+        }
+
+
+def delete(name):
+    # User name is empty
+    if name.strip() != "":
+
+        # Users exists in the data base
+        if users.find({"name": name}).count() == 1:
+
+            users.remove({"name": name})
+            return 200
+        else:
+            return 'Bad request', 400, {
+                'exists-error': 'The users do not exists in the database'
+            }
+    else:
+        return 'Bad request', 400, {
+            'empty-error': 'The name of the user given can not be empty'
+        }
--- test/tests.py
+++ test/tests.py
@@ -14,6 +14,13 @@ class TestUsersApi(unittest.TestCase):
         self.assertEqual(response.status_code, 200, "Should be 200")
         
 
+    def test_path_not_found(self):
+        response = requests.get(self.API_URL+'/user',
+        headers={'Accept': 'application/json'})
+
+        self.assertEqual(response.status_code, 404, "Should be 404")
+    
+
     def test_add_new_user(self):
 
         response = requests.post(self.API_URL+'/users/JesusPaz',
@@ -25,10 +32,53 @@ class TestUsersApi(unittest.TestCase):
         aux = json.loads(data)
         userId = aux["id"]
         numRepos = aux["numRepos"]
-        
+
+        # Used to delete the user and can try the test again
+        response = requests.delete(self.API_URL+'/users/JesusPaz',
+        headers={'Accept': 'application/json'})
+
+        # test deleted ok
+        self.assertEqual(response.status_code, 200, "Should be 200")
+
         self.assertTrue(userId != "" and userId != None, "Can not be empty or None")
         self.assertEqual(numRepos, 22, "Number of repos should be 22")
       
+    def test_add_new_user_empty_name(self):
+
+        response = requests.post(self.API_URL+'/users/ ',
+        headers={'Accept': 'application/json'})
+        
+        self.assertEqual(response.status_code, 400, "Should be 400")
+
+    def test_add_new_user_exists_in_database(self):
+
+        response = requests.post(self.API_URL+'/users/danielq97',
+        headers={'Accept': 'application/json'})
+        
+        self.assertEqual(response.status_code, 400, "Should be 400")
+
+    def test_add_new_user_dont_exists_in_github(self):
+
+        response = requests.post(self.API_URL+'/users/atdgps85632s',
+        headers={'Accept': 'application/json'})
+        
+        self.assertEqual(response.status_code, 400, "Should be 400")
+    
+    def test_delete_user_dont_exist(self):
+
+        response = requests.delete(self.API_URL+'/users/atdgps85632s',
+        headers={'Accept': 'application/json'})
+        
+        self.assertEqual(response.status_code, 400, "Should be 400")
+
+    def test_delete_user_empty_name(self):
+
+        response = requests.delete(self.API_URL+'/users/ ',
+        headers={'Accept': 'application/json'})
+        
+        self.assertEqual(response.status_code, 400, "Should be 400")
+
+    
 
 if __name__ == '__main__':
     unittest.main()
