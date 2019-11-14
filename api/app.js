b7695e8bf609fc217c299dd911b912afb774ae30
juanes-2504@hotmail.com <lasalsaysuuniverso*20182>
2019-11-14 02:56:27 -0500

    Database Mongodb Database 1

--- api/app.js
+++ api/app.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+// Database: Mongo
+const mongoose = require('mongoose')
+// Try to change PASSWORD to process.env.MONGO_ATLAS_PW (EN: variable de entorno)
+mongoose.connect('mongodb+srv://admin:PASSWORD@clusterparcial2-vrxdh.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true }).then(() => {
+  console.log('connection to database establish')
+}).catch(err => {
+  console.log(err)
+  process.exit(-1)
+})
 
 const movieRoutes = require('../routes/movies')
 const reviewRoutes = require('../routes/reviewers')
