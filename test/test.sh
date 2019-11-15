503e5e9baff24eecd7df184849b426b82ae23bc5
gaearaz <azo_67@hotmail.es>
2019-11-15 18:56:09 -0500

    some changes to travis.yml

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-py.test ./test_minimal-200.tavern.yaml -v
-py.test ./test_minimal-400.tavern.yaml -v
-py.test ./test_minimal-404.tavern.yaml -v
+py.test test_minimal-200.tavern.yaml -v
+py.test test_minimal-400.tavern.yaml -v
+py.test test_minimal-404.tavern.yaml -v
