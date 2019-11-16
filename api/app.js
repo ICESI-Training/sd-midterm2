179e102bd7a293f4f9e4a77e16960d998a9a9f72
juanes-2504@hotmail.com <lasalsaysuuniverso*20182>
2019-11-15 20:30:06 -0500

    Travis 9 - We surrender with Travis

--- api/app.js
+++ api/app.js
@@ -5,13 +5,12 @@ const bodyParser = require('body-parser')
 // Database: Mongo
 const mongoose = require('mongoose')
 // Try to change PASSWORD to process.env.MONGO_ATLAS_PW (EN: variable de entorno)
-// mongoose.connect('mongodb+srv://admin:PASSWORD@clusterparcial2-vrxdh.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true }).then(bla => {
-//   console.log('connection to database establish')
-// }).catch(err => {
-//   console.log(err)
-//   process.exit(-1)
-// })
-mongoose.connect('mongodb+srv://admin:PASSWORD@clusterparcial2-vrxdh.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://admin:PASSWORD@clusterparcial2-vrxdh.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true }).then(() => {
+  console.log('connection to database establish')
+}).catch(err => {
+  console.log(err)
+  process.exit(-1)
+})
 
 const movieRoutes = require('../routes/movies')
 
