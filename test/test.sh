3610f047b9c458bfded7f079c2fb26fccaf2bdf6
Andres Zapata <azo_67@hotmail.es>
2019-11-15 22:11:01 -0500

    minor change

--- test/test.sh
+++ test/test.sh
@@ -2,4 +2,4 @@
 python3 ./src/handlers.py & # this is to run travis, comment if you need to run it locally 
 py.test ./test/test_minimal-200.tavern.yaml -v
 py.test ./test/test_minimal-400.tavern.yaml -v
-py.test ./test/test_minimal-404.tavern.yaml -vgit a
+py.test ./test/test_minimal-404.tavern.yaml -v
